5977159dc55a56abccdf99045d473c9191c3f801
cprevite <claudio.previte@unil.ch>
2019-12-03 13:25:41 +0100

    Create Namespace with roxygen2

--- R/package_function.R
+++ R/package_function.R
@@ -1,8 +1,7 @@
 
 #' @title Variables per Country through Year
 #' @description Compute an linetype graph containing the desired variables per desired countries through year
-#' @param ctry A \code{list} of countries to select
-#' region of interest, i.e. [a, b].
+#' @param ctry A \code{list} of countries to selectcoun
 #' @param var A \code{list} of variables to select
 #' @return A \code{plot} containing the line per variables and per countries
 #' @author Claudio Previte
@@ -11,6 +10,38 @@
 #' country_function('Italy', c('Productivity', 'GDP'))
 #' @export
 country_function <- function(ctry, var){
+
+  library(readxl)
+  library(here)
+  library(tidyverse)
+  library(dplyr)
+
+  #Loading the data
+  data <- as.data.frame(read_excel(here('Passport_Stats.xls')))
+
+  #Deleting useless column
+  data <- data[,-c(3:4)]
+
+  #Renaming a certain variable
+  data <- data %>% rename(country = Geography)
+
+  #Tidying the data
+  data <- data %>% gather(c(3:44), key = year, value = values)
+
+  data <- data %>% spread(Category, values)
+
+  colnames(data) <- colnames(data) %>% gsub(pattern = " ", replacement = "_")
+
+  data <- as_tibble(data)
+
+  data$country <- as.factor(data$country)
+
+  data$year <- as.factor(data$year)
+
+  data[, 3:19] <- sapply(data[, 3:19], as.numeric)
+
+  data.long <- gather(data, key="Variable", value="Value", c(3:19))
+### -------------------------------------------------------------------------------------------------
 
   data.long.function <- data.long %>%
     filter(Variable %in% var & Country %in% ctry)
