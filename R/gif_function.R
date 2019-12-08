9756e93c1145a1280671da0f3b9afad4df673557
JEREMY CHOPPE <JEREMY.CHOPPE@UNIL.CH>
2019-12-08 22:22:46 +0100

    comments of gif function

--- R/gif_function.R
+++ R/gif_function.R
@@ -1,26 +1,28 @@
-#' @title Animation of the European indicators over time
-#' @description Produces a GIF about one variable and shows an EU chloropleth map with colors changing accordingly to the data evolution
-#' @param pol_var A \code{list} of any variable to select
+#' @title Evolution of European indicators through time
+#' @description Displays a GIF showing the evolution of one variable through time over an EU chloropleth map
+#' @param var A \code{list} of any variable to select from the dataset
 #' @return An \code{animation} containing the map of Europe based on the variable chosen
 #' @author Jeremy Choppe
 #' @examples
-#' comparison_function('Greenhouse_Gas_Emissions')
-#' comparison_function('CO2_Emissions_from_Transport')
+#' gif_function('Greenhouse_Gas_Emissions')
 #' @import dplyr maptools ggmap hrbrthemes here tidyverse DataExplorer maps tmap mapview leaflet ggplot2 viridis readxl
 #' shiny sf raster spData magick gganimate reshape2 mapproj
 #' @export
 
 gif_function <- function(var) {
-  
+
+  #selecting the desired world area, which is relevant to the dataset
   world1 <- map_data("world")
   world_eu <- world %>% filter(continent == "Europe")
   world_eu <- world_eu[, c(1, 2)]
   world_eu <- left_join(world_eu, data, by = c("name_long" = "country"))
   world_eu <- world_eu[-1,]
 
+  #setting the gif features with some tmap options and along years
   gif <- tm_shape(world_eu) + tm_fill(col = var) + tm_polygons()  +
     tm_facets(along = "year", free.coords = FALSE)
 
+  #defining the dimensions of the gif and where to save the file created
   tmap_animation(
     gif,
     filename = "tmap.gif",
