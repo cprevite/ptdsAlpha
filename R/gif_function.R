06db05f10e9c7b7bc39054e9644f6b95f5f7345e
JEREMY CHOPPE <JEREMY.CHOPPE@UNIL.CH>
2019-12-10 13:48:06 +0100

    update gif

--- R/gif_function.R
+++ R/gif_function.R
@@ -1,34 +1,38 @@
 #' @title Evolution of European indicators through time
-#' @description Displays a GIF showing the evolution of one variable through time over an EU chloropleth map
+#' @description Displays a GIF showing the evolution of one variable through
+#' time over an EU chloropleth map
 #' @param var A \code{list} of any variable to select from the dataset
-#' @return An \code{animation} containing the map of Europe based on the variable chosen
+#' @return An \code{animation} containing the map of Europe based on the
+#' variable chosen
 #' @author Jeremy Choppe
 #' @examples
-#' gif_function('Greenhouse_Gas_Emissions')
-#' @import dplyr maptools ggmap hrbrthemes here tidyverse DataExplorer maps tmap mapview leaflet ggplot2 viridis readxl
-#' shiny sf raster spData magick gganimate reshape2 mapproj
+#' gif_function(data,'Greenhouse_Gas_Emissions')
+#' @import dplyr ggplot2
 #' @export
 
-gif_function <- function(var) {
+gif_function <- function(dataset=data, var='Greenhouse_Gas_Emissions') {
 
   #selecting the desired world area, which is relevant to the dataset
-  world1 <- map_data("world")
-  world_eu <- world %>% filter(continent == "Europe")
+
+  world_eu <- spData::world %>% filter(continent == "Europe")
   world_eu <- world_eu[, c(1, 2)]
-  world_eu <- left_join(world_eu, data, by = c("name_long" = "country"))
+  world_eu <- left_join(world_eu, dataset, by = c("name_long" = "Country"))
   world_eu <- world_eu[-1,]
 
   #setting the gif features with some tmap options and along years
-  gif <- tm_shape(world_eu) + tm_fill(col = var) + tm_polygons()  +
-    tm_facets(along = "year", free.coords = FALSE)
+  gif <-
+    tmap::tm_shape(world_eu) + tmap::tm_fill(col = var) + tmap::tm_polygons()  +
+    tmap::tm_facets(along = "Year", free.coords = FALSE)
 
   #defining the dimensions of the gif and where to save the file created
-  tmap_animation(
+  tmap::tmap_animation(
     gif,
     filename = "tmap.gif",
     delay = 30,
     width = 1000,
     height = 900
   )
+
+ return("tmap.gif")
 
 }
